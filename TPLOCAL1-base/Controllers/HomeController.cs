cbe293ade4c796e436e79b6845427d93ca9877bf
TMornard <47031693+TMornard@users.noreply.github.com>
2024-07-30 11:53:23 +0200

    Add files via upload

---
+++ TPLOCAL1-base/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using TPLOCAL1.Models;
+
+using System;
+
+//Subject is find at the root of the project and the logo in the wwwroot/ressources folders of the solution
+//--------------------------------------------------------------------------------------
+//Careful, the MVC model is a so-called convention model instead of configuration,
+//The controller must imperatively be name with "Controller" at the end !!!
+namespace TPLOCAL1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //method "naturally" call by router
+        public ActionResult Index(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                //retourn to the Index view (see routing in Program.cs)
+                return View();
+            else
+            {
+                //Call different pages, according to the id pass as parameter
+                switch (id)
+                {
+                    case "AvisList":
+                        //TODO : code reading of the xml files provide
+                        List<Opinion> opinionList = new OpinionList().GetAvis("XmlFiles/DataAvis.xml");
+                        ViewBag.opinionList = opinionList;
+                        return View(id);
+                    case "Form":
+                        //TODO : call the Form view with data model empty
+                        return View(id);
+                    default:
+                        //retourn to the Index view (see routing in Program.cs)
+                        return View();
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Index(Formulaire formulaire)
+        {
+            return View(formulaire);
+        }
+
+        public ActionResult ValidationFormulaire(FormCollection formulaire) 
+        {
+            if (1 % 2 == 1)
+            {
+                return View("Validation");
+            }
+            else
+            {
+                return View("Form");
+            }
+        }
+    }
+}
---
+++ TPLOCAL1-base/Models/Formulaire.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class Formulaire
+    {
+        [Required]
+        [Display(Name = "Nom")]
+        public string Nom        { get; set; }
+        [Required]
+        [Display(Name = "Prenom")]
+        public string Prenom     { get; set; }
+        [Required]
+        [Display(Name = "Sexe")]
+        public string Sexe       { get; set; }
+        [Required]
+        [Display(Name = "Adresse")]
+        public string Adresse    { get; set; }
+        [Required]
+        [Display(Name = "Code")]
+        public int    Code       { get; set; }
+        [Required]
+        [Display(Name = "Ville")]
+        public string Ville      { get; set; }
+        [Required]
+        [Display(Name = "Mail")]
+        public string Mail       { get; set; }
+        [Required]
+        [Display(Name = "DateDebut")]
+        public string DateDebut  { get; set; }
+        [Required]
+        [Display(Name = "Formation")]
+        public string Formation  { get; set; }
+        [Required]
+        [Display(Name = "AvisCOBOL")]
+        public string AvisCOBOL  { get; set; }
+        [Required]
+        [Display(Name = "AvisC#")]
+        public string AvisCSharp { get; set; }
+    }
+}
